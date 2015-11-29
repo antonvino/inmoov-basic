299f486116e91ae601e1c712b5595c9fdee10641
antonvino <anton.vinokurov.au@gmail.com>
2015-11-29 15:52:17 +0000

    Basic movements: yes, no, wave hand etc. plus main joints with pitch, yaw, roll

--- inmoov_joints.py
+++ inmoov_joints.py
@@ -1,82 +1,123 @@
 #!/usr/bin/python
 from maestro_lib import Device
+from math import fabs
 
-
-  # <Channels MiniMaestroServoPeriod="80000" ServoMultiplier="1">
-  #   <!--Period = 20 ms-->
-  #   <!--Channel 0-->
-  #   <Channel name="test" mode="Servo" min="256" max="16000" homemode="Ignore" home="256" speed="0" acceleration="0" neutral="3968" range="1905" />
-  #   <!--Channel 1-->
-  #   <Channel name="head up-down" mode="Servo" min="2432" max="5504" homemode="Off" home="2432" speed="0" acceleration="0" neutral="5440" range="1905" />
-  #   <!--Channel 2-->
-  #   <Channel name="head LR RECAL" mode="Servo" min="4544" max="7936" homemode="Off" home="4544" speed="0" acceleration="0" neutral="6720" range="1905" />
-  #   <!--Channel 3-->
-  #   <Channel name="torso (double)" mode="Servo" min="1984" max="9216" homemode="Off" home="1984" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 4-->
-  #   <Channel name="L shldr" mode="Servo" min="7040" max="9216" homemode="Off" home="7040" speed="0" acceleration="0" neutral="8760" range="1905" />
-  #   <!--Channel 5-->
-  #   <Channel name="L shldr fb" mode="Servo" min="3264" max="9792" homemode="Off" home="3264" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 6-->
-  #   <Channel name="L shldr twst" mode="Servo" min="2048" max="9152" homemode="Off" home="2048" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 7-->
-  #   <Channel name="L elbow RECAL" mode="Servo" min="3904" max="5632" homemode="Off" home="3904" speed="0" acceleration="0" neutral="5184" range="1905" />
-  #   <!--Channel 8-->
-  #   <Channel name="R shldr" mode="Servo" min="5888" max="7808" homemode="Off" home="5888" speed="0" acceleration="0" neutral="5888" range="1905" />
-  #   <!--Channel 9-->
-  #   <Channel name="R shldr fb" mode="Servo" min="4096" max="8576" homemode="Off" home="4096" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 10-->
-  #   <Channel name="R shldr twst" mode="Servo" min="2240" max="10240" homemode="Off" home="2240" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 11-->
-  #   <Channel name="R elbow RECAL" mode="Servo" min="5184" max="8384" homemode="Off" home="5184" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 12-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 13-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 14-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 15-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 16-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 17-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 18-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 19-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 20-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 21-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 22-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  #   <!--Channel 23-->
-  #   <Channel name="" mode="Servo" min="3968" max="8000" homemode="Off" home="3968" speed="0" acceleration="0" neutral="6000" range="1905" />
-  # </Channels>
-
-INMOOV_LIMITS = [
-    [0,0],  # test servo
-    [608,1376],  # head up-down
+# positions
+LOW = 0
+MIDDLE = 1
+HIGH = 2
+# values: low, middle, high
+INMOOV_JOINTS = [
+    [0,0,0],  # test servo
+    [608,1000,1376],  # head pitch
+    [1136,1400,1984],  # head yaw
+    [496,1500,2304],  # torso
+    [1760,2000,2304],  # L shoulder roll
+    [816,1600,2448],  # L shoulder pitch
+    [512,1350,2288],  # L shoulder yaw
+    [976,1200,1408],  # L elbow
+    [1472,1600,1952],  # R shoulder roll
+    [1024,1500,2144],  # R shoulder pitch
+    [560,1560,2560],  # R shoulder yaw
+    [1296,1696,2096],  # R elbow
 ]
 
-INMOOV_HEAD_SERVO = 1
+INMOOV_SERVOS = {
+    'head': {
+        'pitch': 1,
+        'yaw': 2
+    },
+    'torso': 3,
+    'left':
+    {
+        'shoulder': {
+            'roll': 4,
+            'pitch': 5,
+            'yaw': 6
+        },
+        'elbow': 7
+    },
+    'right':
+    {
+        'shoulder': {
+            'roll': 8,
+            'pitch': 9,
+            'yaw': 10
+        },
+        'elbow': 11
+    },
+}
+
+# sides
+LEFT = 0
+RIGHT = 1
+
+SIDE_NAMES = {
+    LEFT: 'left',
+    RIGHT: 'right'
+}
+
+POS_NAMES = {
+    MIDDLE: 'middle',
+    LOW: 'low',
+    HIGH: 'high'
+}
 
 servo = Device("/dev/ttyAMA0","/dev/ttyAMA0")
 
 import sys
 from math import floor
 
-def move_head(direction = 'up'):
-    target = None
-    print '[MOVE] Head: {0}'.format(direction)
-    if(direction == 'up'):
-        target = INMOOV_LIMITS[INMOOV_HEAD_SERVO][1]  # upper limit
-    elif(direction == 'center'):
-        target = INMOOV_LIMITS[INMOOV_HEAD_SERVO][0] + int(floor((INMOOV_LIMITS[INMOOV_HEAD_SERVO][1]-INMOOV_LIMITS[INMOOV_HEAD_SERVO][0])/2))
-    elif(direction == 'down'):
-        target = INMOOV_LIMITS[INMOOV_HEAD_SERVO][0]  # lower limit
-    else:
-        print 'Invalid direction'
-    if target is not None:
+def head(part, pos):
+    if pos_valid(pos):
+        print '[MOVE] Head: {0} {1}'.format(part, POS_NAMES[pos])
+        srv = INMOOV_SERVOS['head'][part]
+        target = INMOOV_JOINTS[srv][pos]
         print 'Target: {0}'.format(target)
-        servo.set_target(INMOOV_HEAD_SERVO, target)
+        servo.set_target(srv, target)
+    else:
+        print 'Invalid position'
+
+def torso(pos):
+    if pos_valid(pos):
+        print '[MOVE] Torso: {0}'.format(POS_NAMES[pos])
+        srv = INMOOV_SERVOS['torso']
+        target = INMOOV_JOINTS[srv][pos]
+        print 'Target: {0}'.format(target)
+        servo.set_target(srv, target)
+    else:
+        print 'Invalid position'
+
+def elbow(side, pos):
+    if pos_valid(pos):
+        print '[MOVE] Elbow: {0} {1}'.format(SIDE_NAMES[side], POS_NAMES[pos])
+        srv = INMOOV_SERVOS[SIDE_NAMES[side]]['elbow']
+        target = INMOOV_JOINTS[srv][pos]
+        print 'Target: {0}'.format(target)
+        servo.set_target(srv, target)
+    else:
+        print 'Invalid position'
+
+def shoulder(side, axis, pos, offset = 0):
+    if pos_valid(pos):
+        if(fabs(offset) > 300):
+            offset = 300
+        print '[MOVE] Shoulder: {0} {1} {2}'.format(SIDE_NAMES[side], axis, POS_NAMES[pos])
+        srv = INMOOV_SERVOS[SIDE_NAMES[side]]['shoulder'][axis]
+        target = INMOOV_JOINTS[srv][pos]+offset
+        print 'Target: {0}'.format(target)
+        servo.set_target(srv, target)
+    else:
+        print 'Invalid position'
+
+def pos_valid(pos):
+    valid = False
+    if(pos == LOW or pos == MIDDLE or pos == HIGH):
+        valid = True
+    return valid
+
+def arm_init(side):
+    shoulder(side, 'pitch', MIDDLE)
+    shoulder(side, 'yaw', MIDDLE)
+    shoulder(side, 'roll', LOW)
+    elbow(side, HIGH)
--- inmoov_move.py
+++ inmoov_move.py
@@ -3,88 +3,67 @@ from inmoov_joints import *
 import time
 
 cmd = sys.argv[1]
-if(cmd == "test_head"):
-    print '[CMD] Test head'
-    move_head('up')
-    time.sleep(1)
-    move_head('down')
-    time.sleep(1)
-    move_head('center')
-
-
-# elif(cmd == "lower"):
-#     print '[CMD] Lower legs {0}'.format(legs)
-#     for leg in legs:
-#         lower_leg(leg)
-# elif(cmd == "forward"):
-#     print '[CMD] Rotate legs forward {0}'.format(legs)
-#     for leg in legs:
-#         rotate_leg(leg, "forward")
-# elif(cmd == "back"):
-#     print '[CMD] Rotate legs back {0}'.format(legs)
-#     for leg in legs:
-#         rotate_leg(leg, "back")
-# elif(cmd == "middle"):
-#     print '[CMD] Rotate legs to the center {0}'.format(legs)
-#     for leg in legs:
-#         rotate_leg(leg, "middle")
-# elif(cmd == "init"):
-#     print '[CMD] Reset legs'
-#     init_legs()
-# elif(cmd == "step_fwd"):
-#     print '[CMD] Step forward {0}'.format(legset)
-#     if(legset <= 0 or legset >=11):
-#         legset = 1
-#     step = 0
-#     while(step < legset):
-#         # assuming initially init()
-#         # lift 1 set up
-#         legs = [0,1,2]
-#         for leg in legs:
-#             lift_leg(leg)
-#         time.sleep(0.2)
-#         # move 1 set forward
-#         legs = [0,1,2]
-#         for leg in legs:
-#             rotate_leg(leg,"forward")
-#         time.sleep(0.2)
-#         # lower 1 set down
-#         legs = [0,1,2]
-#         for leg in legs:
-#             lower_leg(leg)
-#         # lift 2 set up (same time)
-#         legs = [3,4,5]
-#         for leg in legs:
-#             lift_leg(leg)
-#         time.sleep(0.2)
-#         # move 1 set to middle (this should move the body forward if there
-#         # is not too much slip)
-#         legs = [0,1,2]
-#         for leg in legs:
-#             rotate_leg(leg,"middle")
-#         time.sleep(0.2)
-#         # move set 2 forward
-#         legs = [3,4,5]
-#         for leg in legs:
-#             rotate_leg(leg,"forward")
-#         time.sleep(0.2)
-#         # lower 2 set down
-#         legs = [3,4,5]
-#         for leg in legs:
-#             lower_leg(leg)
-#         # time.sleep(0.2)
-#         # lift 1 set up
-#         legs = [0,1,2]
-#         for leg in legs:
-#             lift_leg(leg)
-#         time.sleep(0.2)
-#         # move 2 set to the middle (this should move the body forward)
-#         legs = [3,4,5]
-#         for leg in legs:
-#             rotate_leg(leg,"middle")
-#         time.sleep(0.2)
-#         # increase the step count
-#         step+=1
-#
-#     # stop - init legs
-#     init_legs()
+if(cmd == "yes"):
+    print '[CMD] Yes. Nodding.'
+    head('pitch', HIGH)
+    time.sleep(0.3)
+    head('pitch', LOW)
+    time.sleep(0.3)
+    head('pitch', HIGH)
+    time.sleep(0.3)
+    head('pitch', LOW)
+    time.sleep(0.2)
+    head('pitch', MIDDLE)
+elif(cmd == "no"):
+    print '[CMD] No. Shaking head.'
+    head('yaw', LOW)
+    time.sleep(0.3)
+    head('yaw', HIGH)
+    time.sleep(0.3)
+    head('yaw', LOW)
+    time.sleep(0.3)
+    head('yaw', HIGH)
+    time.sleep(0.2)
+    head('yaw', MIDDLE)
+elif(cmd == "wave_left"):
+    print '[CMD] Waving left hand.'
+    # up-shoulder up PITCH is fwd-back shoulder
+    shoulder(LEFT, 'pitch', HIGH)
+    # twist-shoulder middle
+    shoulder(LEFT, 'yaw', MIDDLE)
+    # elbow middle
+    elbow(LEFT, MIDDLE)
+    # side-shoulder low
+    shoulder(LEFT, 'roll', LOW)
+    time.sleep(8)
+    # twist-shoulder left-right
+    shoulder(LEFT, 'yaw', HIGH)
+    time.sleep(4)
+    shoulder(LEFT, 'yaw', LOW)
+    time.sleep(4)
+    shoulder(LEFT, 'yaw', HIGH)
+    time.sleep(4)
+    shoulder(LEFT, 'yaw', LOW)
+    time.sleep(4)
+    # back to normal
+    arm_init(LEFT)
+elif(cmd == "hands_up"):
+    # left up-shoulder up
+    # right up-shoulder up
+    # left side shoulder low
+    # left side shoulder low
+    # left twist-shoulder middle
+    # right twist shoulder middle
+    pass
+elif(cmd == "hands_span"):
+    # shoulders up
+    # elbows down
+    # shoulder-twist forward
+    pass
+elif(cmd == "hands_wave"):
+    # hands up then shoulders low-high, high-low
+    # so that hands move in same directions together
+    pass
+elif(cmd == "arm_init"):
+    arm_init(LEFT)
+    arm_init(RIGHT)
